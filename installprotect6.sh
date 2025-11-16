8ffefa770cb2f0d61b8c180c686ddafb0e1aceb7
sennn401-prog <sennn401@gmail.com>
2025-11-16 18:41:24 +0700

    Update installprotect6.sh

--- installprotect6.sh
+++ installprotect6.sh
@@ -55,7 +55,7 @@ class IndexController extends Controller
         // 🔒 Anti akses menu Settings selain user ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '𝖺𝗄𝗌𝖾𝗌 𝖽𝗂𝗍𝗈𝗅𝖺𝗄 𝗉𝗋𝗈𝗍𝖾𝖼𝗍 𝖻𝗒 sennnyxz!❌');
+            abort(403, 'AKSES DI TOLAK PROTECT BY @sennnyxz❌');
         }
 
         return $this->view->make('admin.settings.index', [
@@ -75,7 +75,7 @@ class IndexController extends Controller
         // 🔒 Anti akses update settings selain user ID 1
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '𝖺𝗄𝗌𝖾𝗌 𝖽𝗂𝗍𝗈𝗅𝖺𝗄 𝗉𝗋𝗈𝗍𝖾𝖼𝗍 𝖻𝗒 sennnyxz!');
+            abort(403, 'AKSES DI TOLAK PROTECT BY @sennnyxz!');
         }
 
         foreach ($request->normalize() as $key => $value) {
